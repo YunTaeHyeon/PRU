89d04248b8d8e86f59f3d1de182dc5b2ba1ff9a0
YunTaeHyeon <t05h19@naver.com>
2023-05-14 20:05:36 +0900

    spring

---
+++ spring/src/main/java/com/pru/demo/PruApplication.java
@@ -0,0 +1,13 @@
+package com.pru.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruApplication.class, args);
+	}
+
+}
