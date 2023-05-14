06ed4288c00b85bf635326b0333e439ae4c59f10
YunTaeHyeon <t05h19@naver.com>
2023-05-14 23:11:30 +0900

    server develop

---
+++ PRUspring/src/main/java/com/spring/PRUspring/PrUspringApplication.java
@@ -0,0 +1,13 @@
+package com.spring.PRUspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrUspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrUspringApplication.class, args);
+	}
+
+}
---
+++ PRUspring/src/test/java/com/spring/PRUspring/PrUspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.PRUspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrUspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
